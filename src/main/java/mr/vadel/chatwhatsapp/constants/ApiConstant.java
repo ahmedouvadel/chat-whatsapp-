9993a4cf95c88d27388cc73fe4a4a12c2e3f78f5
ahmedouvadel <100805939+ahmedouvadel@users.noreply.github.com>
2025-01-31 15:29:36 +0000

    fix API-CONSTANT

--- src/main/java/mr/vadel/chatwhatsapp/constants/ApiConstant.java
+++ src/main/java/mr/vadel/chatwhatsapp/constants/ApiConstant.java
@@ -5,8 +5,8 @@ public class ApiConstant {
     public static final String API_URL = "/api/v1";
     public static final String CHAT_URL = API_URL + "/chats";
     public static final String MESSAGE_URL = API_URL + "/messages";
-    public static final String MESSAGE_URL_UPLOAD_MEDIA = MESSAGE_URL + "/upload-media";
-    public static final String GET_MESSAGES = MESSAGE_URL + "/chat/{chat-id}";
+    public static final String MESSAGE_URL_UPLOAD_MEDIA = "/upload-media";
+    public static final String GET_MESSAGES = "/chat/{chat-id}";
     public static final String CONSUMES_FILE = "multipart/form-data";
     public static final String USER_URL = API_URL + "/users";
 
